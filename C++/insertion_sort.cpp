c373862e94e530353c4b5bacd59c1c06796b5fb3
h4med <hamed.d@gmail.com>
2023-04-04 22:39:13 +0330

    add insertion sort c++

---
+++ C++/insertion_sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    for(int idx = 1; idx <size(arr); ++idx) {
+        int item = arr[idx];
+        while (idx > 0 && item < arr[idx-1])
+        {
+            arr[idx] = arr[idx - 1];
+            --idx;
+        }
+        arr[idx] = item;
+    }
+}
+
+int main() {
+ vector<int> arr = {5, 2, -3, 4, 6, -7, 1, 9, 12, 5, -6};
+
+    cout << "input: ";
+    for(int num: arr){
+        cout << num << " ";
+    }    
+    cout << endl;
+
+    insertionSort(arr);
+
+    cout << "Output: ";
+    for (int i = 0; i < arr.size(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
