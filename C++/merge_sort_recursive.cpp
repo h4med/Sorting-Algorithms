3819bac0d30052554be45fa704d6713a043ca8ed
h4med <hamed.d@gmail.com>
2023-04-15 22:24:35 +0330

    merge C++ added

--- C++/merge_sort_recursive.cpp
+++ C++/merge_sort_recursive.cpp
@@ -3,21 +3,64 @@
 
 using namespace std;
 
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> leftArr(n1);
+    vector<int> rightArr(n2);
+
+    for (int i = 0; i < n1; ++i)
+        leftArr[i] = arr[left + i];
+
+    for (int j = 0; j < n2; ++j)
+        rightArr[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] < rightArr[j]) {
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
+
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
 
 int main() {
     vector<int> arr = {5, 12, 3, 4, 7, 1, 0, 6, 19, 8, 13, 4, 2, 10, 16};
+    int n = arr.size();
 
     cout << "input: ";
-    for(int num: arr){
-        cout << num << " ";
-    }    
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
     cout << endl;
 
-    int ops = countingSort(arr);
-    cout << "No of operations: " << ops << endl;
+    mergeSort(arr, 0, n - 1);
 
     cout << "Output: ";
-    for (int i = 0; i < arr.size(); ++i) {
+    for (int i = 0; i < n; ++i) {
         cout << arr[i] << " ";
     }
     cout << endl;
