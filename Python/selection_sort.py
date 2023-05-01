4e4ff4da6e38c72d33d912f814fe6b869d6c1b66
h4med <hamed.d@gmail.com>
2023-05-01 13:18:45 +0330

    rewriting selection py with functions

--- Python/selection_sort.py
+++ Python/selection_sort.py
@@ -1,17 +1,21 @@
+def findSmallestIdx(arr):
+    smallest = arr[0]
+    smallestIndex = 0
+    for i in range(1, len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            smallestIndex = i
+
+    return smallestIndex
+
+def selectionSort(arr):
+    newArr = [0] * len(arr)
+    for i in range(len(arr)):
+        smallestIdx = findSmallestIdx(arr)
+        newArr[i] = arr.pop(smallestIdx)
+    
+    return newArr
+
 A = [5, 10, 5, 2, 5, 9, 2, 6, 5, 3, 2]
-#A = [0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 10, 12, 13, 16, 19]
-A = A[-1::-1]
-n = len(A)
 print("Input:", A)
-
-ops = 0
-for i in range(n):
-    index = i
-    for j in range(i+1, n):
-        ops += 1
-        if A[index] > A[j]:
-            index = j
-    A[i], A[index] = A[index], A[i]
-
-print("No of Operations: ", ops)
-print("Output:", A)
+print("Output:", selectionSort(A))
