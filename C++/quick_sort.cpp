fa6b1a1043231d335452b8f671a036d366427b2f
h4med <hamed.d@gmail.com>
2023-05-04 12:14:05 +0330

    quickSort in C++ added

--- C++/quick_sort.cpp
+++ C++/quick_sort.cpp
@@ -1,67 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 using namespace std;
 
-void displayArray(const int* arr, int size) {
-    int i = 0;
-    printf("[");
-    for(i = 0; i < size; ++i) {
-        printf(" %d ", arr[i]);
-    }
-    printf("]\n");
-};
-
-void swapInt(int& var1, int& var2) {
-    int temp = var1;
-    var1 = var2;
-    var2 = temp;
-};
-
-void quickSort(vector<int>& arr, int size) {
-    if(size < 2)
-        return;
+template <typename T>
+vector<T> quickSort(const vector<T>& arr) {
+    if(arr.size() < 2)
+        return arr;
     
-    int pivot = arr[rand() % size];
-    // cout << "pivot: " << pivot << endl;
-    // cout << "rand: " << rand() << endl;
-    int lower = 0;
-    int upper = size - 1;
+    const T* pivot = &arr.front() + arr.size() / 2 - 1;
+    vector<T> lessTP;
+    vector<T> greaterTP;
 
-    while(lower < upper){
-
-        while(arr[lower] < pivot)
-            lower++;
-
-        while(arr[upper] > pivot)
-            upper--;
-        
-
-
+    for(const T* item = &arr.front(); item <= &arr.back(); item++){
+        if(item == pivot) continue;
+        if(*item <= *pivot) lessTP.push_back(*item);
+        else greaterTP.push_back(*item);
     }
+
+    vector<T> sorted_lessTP = quickSort(lessTP);
+    vector<T> sorted_greaterTP = quickSort(greaterTP);
+
+    sorted_lessTP.push_back(*pivot);
+    sorted_lessTP.insert(sorted_lessTP.end(), sorted_greaterTP.begin(), sorted_greaterTP.end());
+
+    return sorted_lessTP;
 }
 
 int main() {
-    // int numbers[] = {5, 10, 5, 2, 5, 9, 2, 6, 5, 3, 2};
-
-    // int size = sizeof(numbers)/sizeof(int);
-    // int out[size];
     vector<int> arr = {5, 12, 3, 4, 7, 1, 0, 6, 19, 8, 13, 4, 2, 10, 16};
-    int n = arr.size();
-
+   
     cout << "input: ";
     for(int num: arr){
         cout << num << " ";
     }    
     cout << endl;
 
-    srand((unsigned int)time(0));
-    quickSort(arr, n);
-    // printf("No of operations: %d\n", ops);
+    vector<int> sorted = quickSort(arr);
 
-    cout << "input: ";
-    for(int num: arr){
+    cout << "Output: ";
+    for(int num: sorted){
         cout << num << " ";
     }    
     cout << endl;
